f1ba9b7c118da12c6aba281d3444ea26614d305b
frikky <frikky@dev.wsg.int>
2017-05-09 12:15:23 +0100

    Configuration to work with smtpd.SMTPServer

--- smtpd_senddata.py
+++ smtpd_senddata.py
@@ -5,40 +5,43 @@ import getpass
 from email.mime.text import MIMEText
 
 """
-Fill in the blanks to make it work :) 
-Currently running under the hotmail domain (smtp.live.com:587). 
-Can be changed under run_server().
-
-Mail changes are done under add_receipients.
 """
 
 # Mail sender class. Not receiving shit for now, but smtp_custom.py is server. Tweak \o/
 class mail_send(object):
 
     # Initializer for the class. This is where the username etc should be checked.
-    def __init__(self):
-        self.sender = ""
-        self.receiver = ""
-        self.msg = MIMEText('This is the body of test message.')
+    def __init__(self, msg):
+        self.msg = MIMEText(msg) 
 
     # Adds the info needed for the mail before sending it.
-    def add_recipients(self):
+    def add_recipients(self, subject):
         self.msg['To'] = email.utils.formataddr(('Recipient', self.receiver))
         self.msg['From'] = email.utils.formataddr(('Author', self.sender))
-        self.msg['Subject'] = 'Hello im sending a mail to test this thingy'
+        self.msg['Subject'] = subject
+        print self.msg["Subject"]
 
     # Checks credentials for the user. If sender is not defined (hardcoded) it will be prompted.
-    def check_credentials(self):
-        self.sender = raw_input("Sender mail: ") if not self.sender else 0 
-        self.receiver= raw_input("Receiver mail: ") if not self.receiver else 0 
-        self.password = getpass.getpass("Password: ")
+    def check_credentials(self, receiver):
+        # Change here
+        self.sender = ""
+        self.receiver = receiver
+        print "Sending from %s to %s" % (self.sender, self.receiver)
+
+        if not self.receiver:
+            print "No receiver address: %s" % self.receiver
+            exit()
+
+        self.password = getpass.getpass("Input password for %s: " % self.sender)
 
     # This is where the magic happens lmao.
     def run_server(self):
         target = "smtp.live.com"
         target_port = 587
+
+        print self.sender
         print "Connecting to %s:%d" % (target, target_port)
-        server = smtplib.SMTP(target, target_port)
+        server = smtplib.SMTP(target, target_port, timeout=5)
 
         # Remove # to enable commenting
         #server.set_debuglevel(True) 
@@ -64,13 +67,15 @@ class mail_send(object):
 
         try:
             server.sendmail(self.sender, self.receiver, self.msg.as_string())
-        finally:
-            print "Mail sent to %s" % self.receiver
+        except smtplib.SMTPServerDisconnected as e:
+            print "Error: %s" % e
             server.quit()
+
+        print "Mail sent to %s" % self.receiver
 
 
 if __name__ == "__main__":
-    sender = mail_send()
-    sender.check_credentials()
-    sender.add_recipients()
+    sender = mail_send("HELLO")
+    sender.check_credentials("")
+    sender.add_recipients("HELLO THIS IS TEST")
     sender.run_server()
